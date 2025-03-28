513317b70dd9670ba3b634adc52a21d07aa6432b
Camilo Londoño <bclondonob@uqvirtual.edu.co>
2025-03-28 00:10:45 -0500

    Cree las clases cliente y empleado para gestionar alguna relacion en el futuro, cada clase tiene su documentacion

--- src/main/java/com/laboratorio/Empleado.java
+++ src/main/java/com/laboratorio/Empleado.java
@@ -1,38 +1,77 @@
 package com.laboratorio;
 
+/*
+ * Clase empleado para construir un obtejo con datos especificos
+ */
 public class Empleado {
 
+    /*
+     * Atributos de la clase
+     */
     public String name, id;
     public double salary;
 
+    /**
+     * Constructor con los atributos de la clase
+     *
+     * @param id
+     * @param name
+     * @param salary
+     */
     public Empleado(String id, String name, double salary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
     }
 
+    /*
+     * Get para modificar un dato
+     */
     public String getName() {
         return name;
     }
 
+    /*
+     * Set para obtener un dato
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /*
+     * Get para modificar un dato
+     */
     public String getId() {
         return id;
     }
 
+    /*
+     * Set para obtener un dato
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /*
+     * Get para modificar un dato
+     */
     public double getSalary() {
         return salary;
     }
 
+    /*
+     * Set para obtener un dato
+     */
     public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    /*
+     * Constructor con el toString
+     */
+    @Override
+    public String toString() {
+        return "Empleado [name=" + name + ", id=" + id + ", salary=" + salary + "]";
     }
 
 }
